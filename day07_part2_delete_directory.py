7f9f73aa0241f574246de070e7e9bf8c7f3c4eab
Henry Backman <henry.backman@gmail.com>
2022-12-11 01:12:29 +0100

    day07

--- day07_part2_delete_directory.py
+++ day07_part2_delete_directory.py
@@ -27,10 +27,12 @@ class Directory():
 
 def main():
     directory_repository = DirectoryRepository()
+    root_directory = None
     with open('data/day07_commands.data') as f:
         row = next(f)
         current_directory = Directory('/')
         directory_repository.add_directory(current_directory)
+        root_directory = current_directory
         for row in f:
             if row.startswith('$ ls'):
                 continue
@@ -49,23 +51,26 @@ def main():
                 current_directory.add_file_size(file_size)
             pass
 
-    SIZE_LIMIT = 100000
-    directories_below_limit = []
-    for directory in directory_repository.directories:
-        if directory.get_size() >= SIZE_LIMIT:
-            continue
-        directories_below_limit.append(directory)
+    TOTAL_DISK_SPACE = 70000000
+    MINIMUM_SPACE_REQUIRED = 30000000
+    ADDITIONAL_SPACE_NEEDED = MINIMUM_SPACE_REQUIRED - (TOTAL_DISK_SPACE - root_directory.get_size())
+    smallest_directory_size_to_delete = float('inf')
 
-    sum_of_sizes = 0
-    for directory in directories_below_limit:
-        sum_of_sizes += directory.get_size()
+    for directory in directory_repository.directories:
+        directory_size = directory.get_size()
+        if directory_size < ADDITIONAL_SPACE_NEEDED:
+            continue
+        if directory_size < smallest_directory_size_to_delete:
+            smallest_directory_size_to_delete = directory_size
+
+
 
     # with open('day07_directories', 'w') as f:
     #     for directory in directory_repository.directories:
     #         f.write(f'{directory.name} - {directory.get_size()}\n')
 
 
-    print(f"answer: {sum_of_sizes}")
+    print(f"answer: {smallest_directory_size_to_delete}")
 
 if __name__ == "__main__":
     main()
