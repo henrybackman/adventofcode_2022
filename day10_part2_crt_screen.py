b8cbbd3b095210e832a7a2562dece08d5c4b2ab6
Henry Backman <henry.backman@gmail.com>
2022-12-19 17:24:27 +0200

    day10

---
+++ day10_part2_crt_screen.py
@@ -0,0 +1,60 @@
+CYCLE_TIMES = {
+    "noop": 1,
+    "addx": 2
+}
+
+class Instruction():
+    def __init__(self, type) -> None:
+        self.type = type
+        self.add_value = 0
+        self.cycles_left = CYCLE_TIMES[type]
+        self.is_completed = False
+
+    def run_cycle(self, X):
+        self.cycles_left -= 1
+        if self.cycles_left == 0:
+            self.is_completed = True
+            X = self.execute_instruction(X)
+        return X
+    
+    def execute_instruction(self, X):
+        if self.type == "addx":
+            X += self.add_value
+        return X
+
+def main():
+    instructions = []
+    X = 1
+    with open('data/day10_instructions.data') as f:
+        for row in f:
+            instruction_type = row[0:4]
+            intrs = Instruction(type=instruction_type)
+            if intrs.type == "addx":
+                intrs.add_value = int(row[5:-1])
+            instructions.append(intrs)
+
+    cycle_count = 0
+    ROW_LENGTH = 40
+    row = ""
+
+    instruction = None
+    while instructions or instruction:
+        if not instruction:
+            instruction = instructions.pop(0)
+        cycle_count += 1
+        pixel = "."
+        pixel_position = (cycle_count - 1) % ROW_LENGTH
+        if pixel_position == X or \
+            pixel_position == X - 1 or \
+            pixel_position == X + 1:
+            pixel = "#"
+        row += pixel
+        if len(row) == ROW_LENGTH:
+            print(row)
+            row = ""
+        X = instruction.run_cycle(X)
+        if instruction.is_completed:
+            instruction = None
+
+if __name__ == "__main__":
+    main()
