5decd9ae5d1eda071fb78864e7e882e2adae53f3
Henry Backman <henry.backman@gmail.com>
2023-12-01 16:09:15 +0100

    wip

--- day16_part1_valves.py
+++ day16_part1_valves.py
@@ -2,61 +2,78 @@ import re
 import math
 from collections import deque
 from icecream import ic
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass(frozen=True)
+class Room():
+    id: str
+    flow_rate: int
+    has_flow_rate: bool
+    connected_rooms: List[str] = field(default_factory=list)
+    paths: dict = field(default_factory=dict)
 
 def get_data():
-    data = []
+    rooms = {}
     with open('data/day16_valves.data') as f:
         for row in f:
-            valve_name = re.search(r'(?<=Valve )..', row).group()
-            flow_rate = re.search(r'(?<=has flow rate=)\d+', row).group()
+            id = re.search(r'(?<=Valve )..', row).group()
+            flow_rate = int(re.search(r'(?<=has flow rate=)\d+', row).group())
             s = re.search(r'(?<=tunnels lead to valves ).*', row)
-            connected_valves = None
+            connected_rooms = None
             if s:
-                connected_valves = s.group().split(', ')
-            data.append((valve_name, flow_rate, connected_valves))
-    return data
+                connected_rooms = s.group().split(', ')
 
-def shortest_path(graph, start, end):
-    ic(graph)
-    dist = {start: [start]}
+            room = Room(
+                id=id, 
+                flow_rate=flow_rate, 
+                has_flow_rate=flow_rate > 0,
+                connected_rooms=connected_rooms
+            )
+
+            rooms[id] = room
+            
+    return rooms
+
+def shortest_path(rooms, start, end):
+    paths = {start: [start]}
     q = deque([start])
-    while len(q):
+    while len(q): # nodes left to check
         ic(q)
-        at = q.popleft()
-        ic(at)
-        if not graph[at]:
+        current_node = q.popleft()
+        ic(current_node)
+        if not rooms[current_node]:
             continue
-        for next in graph[at]:
-            ic(next)
-            if next not in dist:
-                dist[next] = dist[at] + [next]
-                ic(dist[next])
-                q.append(next)
-    return dist.get(end)
+        for child_node in rooms[current_node]: # loop through all child nodes
+            ic(child_node)
+            if child_node not in paths: # if node seen first time, this is the shortest path to it
+                paths[child_node] = paths[current_node] + [child_node]
+                ic(paths[child_node])
+                q.append(child_node)
+    return paths.get(end)
 
-def create_graph(d):
-    graph = {}
-    for valve, _, valves in d:
-        graph[valve] = valves
-    return graph
-
-def get_closed_valves(d):
-    valves = []
-    for valve, flow_rate in d:
-        if flow_rate:
-            valves.append(valve)
-    return valves
+def populate_paths(rooms: List[Room]):
+    for room in rooms:
+        paths = {room.id: [room.id]}
+        q = deque([room])
+        while len(q): # rooms left to check
+            current_room = q.popleft()
+            if not room.connected_rooms:
+                continue
+            for connected_room_id in current_room.connected_rooms: # loop through all connected rooms
+                connected_room = rooms[connected_room_id]
+                if connected_room_id not in paths: # if room seen first time, this is the shortest path to it
+                    paths[connected_room_id] = paths[current_room.id] + [connected_room_id]
+                    q.append(connected_room)
+        room.paths = paths
 
 def main():
     ic('getting data')
-    data = get_data()
-    ic(data)
-    graph = create_graph(data)
-    ic(graph)
-    valves_to_open = get_closed_valves(data)
-    ic(valves_to_open)
-    ic(shortest_path(graph, 'AA', 'CC'))
-    ic(shortest_path(graph, 'CC', 'AA'))
+    rooms = get_data()
+    # populate all shortest paths
+    for room in rooms.values():
+        room.paths = all_shortest_paths(rooms, room)
+    ic(rooms)
 
 
 # at each node can either open the valve or not
